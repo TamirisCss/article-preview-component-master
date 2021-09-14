b8c3ca7e20369fcbd4f2c0190bd91c862352c1b2
Tamiris Cristine <tamiris.css@gmail.com>
2021-09-14 10:43:17 +0200

    applying js

---
+++ share.js
@@ -0,0 +1,6 @@
+const shareArrow = document.querySelector(".share");
+const shareBox = document.querySelector(".box-share");
+
+shareArrow.addEventListener("click", () => {
+    shareBox.classList.toggle("show");
+})
